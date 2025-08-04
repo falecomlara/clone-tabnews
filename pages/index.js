4a07ce35c5911e87215951c2fa05949c4db0da78
Eduardo Lara <54687237+falecomlara@users.noreply.github.com>
2025-08-04 03:28:38 +0000

    versao com novas frases

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,42 @@
 function Home() {
-    return <><h1><center>Minhas mensagens honestas neste curso:</center></h1><p /><h2>
-        git commit -m "funciona, mas não sei por quê"<br />
+  return (
+    <>
+      <h1>
+        <center>Minhas mensagens MAIS honestas no Curso.Dev:</center>
+      </h1>
+      <p />
+      <h2>
+        git commit -m "funcionou, mas não sei por quê"<br />
         git commit -m "commitando antes que eu estrague ainda mais"<br />
         git commit -m "não me pergunte como isso funciona"<br />
         git commit -m "arrumei tudo, acho"<br />
         git commit -m "removi aquele .next que atrapalhava"<br />
         git commit -m "a culpa é do front"<br />
         git commit -m "versão final FINAL mesmo"<br />
-        git commit -m "agora vai"<br />
-        git commit -m "correção temporária permanente"<br />
+        git commit -m "agora vai. versão final-02"<br />
+        git commit -m "correção temporária"<br />
         git commit -m "eu sabia, Deus Sabe, você não sabe"<br />
         git commit -m "deixando um problema para o meu EU do futuro resolver"<br />
-        git commit -m "desisto, toma esse código aí"<br />
+        git commit -m "tá funcionando até aqui. não mexe!"<br />
         git commit -m "commitando às cegas, fé no pai"<br />
-        git commit -m "não entendi, mas tá funcionando"<br />
+        git commit -m "parou e agora está funcionando"<br />
         git commit -m "tentativa de conserto nº 7"<br />
-        git commit -m "melhorei a gambiarra anterior"<br /></h2>
-        git commit -m "sim, sou eu de novo"<br/>
-        git commit -m "tudo sob controle (mentira)"<br/>
-        git commit -m "só mais esse ajuste, prometo"<br/>
-        git commit -m "funciona no meu PC"<br/>
-        git commit -m "estava funcionando ontem"<br/>
-        git commit -m "vamos fingir que isso nunca existiu"<br/>
-        git commit -m "versão estável... até a próxima"<br/>
-        git commit -m "testando o voltando no tempo"<br/>
-        git commit -m "não toque nesse arquivo, por favor"<br/>
-        git commit -m "commitado no Curso.Dev"<br/>
-        git commit -m "parece estar certo"<br/>
-        git commit -m "é uma jornada, não uma corrida"<br/></h2>
+        git commit -m "melhorei a gambiarra anterior"<br />
+        git commit -m "sim, sou eu de novo"<br />
+        git commit -m "tudo sob controle"<br />
+        git commit -m "só mais esse ajuste, prometo"<br />
+        git commit -m "funcionou no meu PC"<br />
+        git commit -m "estava funcionando ontem"<br />
+        git commit -m "vamos fingir que isso nunca existiu"<br />
+        git commit -m "versão estável... até a próxima"<br />
+        git commit -m "testando o voltando no tempo"<br />
+        git commit -m "não toque nesse arquivo, por favor"<br />
+        git commit -m "commitado no Curso.Dev"<br />
+        git commit -m "parece estar certo"<br />
+        git commit -m "é uma jornada, não uma corrida"<br />
+      </h2>
+    </>
+  );
 }
 
 export default Home;
