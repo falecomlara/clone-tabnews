28be881b3ee4fbe812479ab83d3d238fc6918035
Eduardo Lara <54687237+falecomlara@users.noreply.github.com>
2025-08-04 03:06:18 +0000

    atualizando o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 function Home() {
-    return <h1><center>Minhas mensagens honestas neste curso:<br/>
-git commit -m "funciona, mas não sei por quê"<br/>
-git commit -m "jeitinho brasileiro aplicado com sucesso"<br/>
-git commit -m "commitando antes que eu estrague ainda mais"<br/>
-git commit -m "não me pergunte como isso funciona"<br/>
-git commit -m "arrumei tudo, acho"<br/>
-git commit -m "removi aquele if que atrapalhava"<br/>
-git commit -m "a culpa é do front"<br/>
-git commit -m "versão final FINAL mesmo"<br/>
-git commit -m "agora vai 🙏"<br/>
-git commit -m "correção temporária permanente"<br/>
-git commit -m "desisto, toma esse código aí"<br/>
-git commit -m "commitando às cegas, fé no pai"<br/>
-git commit -m "não entendi, mas tá funcionando"<br/>
-git commit -m "tentativa de conserto nº 7"<br/>
-git commit -m "melhorei a gambiarra anterior"<br/>
-    </center></h1>
+    return <><h1><center>Minhas mensagens honestas neste curso:</center></h1><p /><h2>
+        git commit -m "funciona, mas não sei por quê"<br />
+        git commit -m "jeitinho brasileiro aplicado com sucesso"<br />
+        git commit -m "commitando antes que eu estrague ainda mais"<br />
+        git commit -m "não me pergunte como isso funciona"<br />
+        git commit -m "arrumei tudo, acho"<br />
+        git commit -m "removi aquele if que atrapalhava"<br />
+        git commit -m "a culpa é do front"<br />
+        git commit -m "versão final FINAL mesmo"<br />
+        git commit -m "agora vai"<br />
+        git commit -m "correção temporária permanente"<br />
+        git commit -m "desisto, toma esse código aí"<br />
+        git commit -m "commitando às cegas, fé no pai"<br />
+        git commit -m "não entendi, mas tá funcionando"<br />
+        git commit -m "tentativa de conserto nº 7"<br />
+        git commit -m "melhorei a gambiarra anterior"<br /></h2></>
 }
 
 export default Home;
